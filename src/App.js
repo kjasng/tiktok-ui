2b13110b007f4be67c8622c36b7035046bedc49a
kjasng <manhnguyenls131@gmail.com>
2024-10-02 20:54:43 +0700

    Configuaration Router/Layout for App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
-import Button from './Components/Button';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/Components/Layout';
+import { HeaderOnly } from '~/Components/Layout';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <Button />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.layout === null ? Fragment : HeaderOnly || DefaultLayout
+            const Page = route.component
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={<Layout><Page /></Layout>}
+              >
+
+              </Route>
+            )
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
